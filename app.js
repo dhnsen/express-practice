2c77255ba13dce073d9430fe2898e08f5cea96bc
Dan H <spag1024@hotmail.com>
2019-07-24 05:40:38 -0500

    installed nodemon and added a script to start it

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log('Listening on port ' + PORT));
 
-app.get()
